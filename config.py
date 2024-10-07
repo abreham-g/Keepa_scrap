3e58ad58095ffcadda56862b56dfc21c6f422ff0
abreham-g <aynuyeabresh@gmail.com>
2024-10-07 10:09:41 +0300

    feat:keepa scrap fixed

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 import os
 
 class Config:
-    KEEP_API_KEY = os.getenv('KEEP_API_KEY', 'ftl9rmbprserlkqios5sv3md5bfrf5jpu2uahr7k6r854i78o56h939i2pcpv8r0')
+    KEEP_API_KEY = os.getenv('KEEP_API_KEY', '****')
     
